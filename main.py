495e7edcccbdbc919e2fe883426707386bb9461e
Mrahadyanbangkit <146795076+Mrahadyanbangkit@users.noreply.github.com>
2023-12-22 11:18:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import os
 from PIL import Image
 import numpy as np
 import tensorflow
-from keras.models import load_model
-from keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array
 
 app = Flask(__name__)
 app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg'])
@@ -101,7 +101,10 @@ def classify_semaphore():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
+
+
 
 # @app.route('/')
 # def index():
